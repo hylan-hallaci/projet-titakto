043d946d2f01e8d9c7f16cd71935e937c8394f4f
GAO <hylan.hallaci@laplateforme.io>
2024-11-05 14:45:04 +0100

    debut du projegt initialisations des boutons et des croix

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-hassoul
+import tkinter
+#detection des bouttons
+def symbole(r,c):
+    print("click",r,c)
+
+    clicked_buttons = buttons[c][r]
+    clicked_buttons.config(text="X")
+    
+
+#grille
+def grille():
+    for column in range(3):
+        buttons_in_cols=[]
+        for row in range (3):
+            button=tkinter.Button(root,font=("arial",50),
+                                  width=5, 
+                                  command = lambda r = row, c=column :symbole(r,c))
+            
+            button.grid(row=row , column=column)
+            buttons_in_cols.append(button)
+        buttons.append(buttons_in_cols)
+
+#stockage
+buttons= []
+
+
+
+#fenetre 
+root = tkinter.Tk()
+#personnalisation 
+root.title("jeu trop bien")
+root.minsize(500,393)
+
+grille()
+root.mainloop()

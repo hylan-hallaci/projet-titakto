f1486e758d3674f44ebf919d5f771d8cdd5521f1
lucas-giaimo <lucas.giaimo@laplateforme.io>
2024-11-05 12:59:18 +0100

    victory_if

--- main.py
+++ main.py
@@ -1,8 +1,103 @@
-<<<<<<< HEAD
-hassoul
 
-tache pour voiture 
-print ("ceci est un test")
-=======
-test
->>>>>>> 5d02947eaa4ff4d25e8b6caad921a188262a6127
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+c1=0
+c2=0
+c3=0
+c4=0
+c5=0
+c6=0
+c7=0
+c8=0
+c9=0
+XS=0
+OS=0
+
+def victory():
+    if c1==1 and c2==1 and c3==1:
+        return 
+    elif c4==1 and c5==1 and c6==1:
+        return
+    elif c7==1 and c8==1 and c9==1:
+        return
+    elif c1==1 and c4==1 and c7==1:
+        return
+    elif c2==1 and c5==1 and c8==1:
+        return
+    elif c3==1 and c6==1 and c9==1:
+        return
+    elif c1==1 and c5==1 and c9==1:
+        return
+    elif c3==1 and c5==1 and c7==1:
+        return
+    else:
+        print("manche suivante")
+        
+    if c1==2 and c2==2 and c3==2:
+        return 
+    elif c4==2 and c5==2 and c6==2:
+        return
+    elif c7==2 and c8==2 and c9==2:
+        return
+    elif c1==2 and c4==2 and c7==2:
+        return
+    elif c2==2 and c5==2 and c8==2:
+        return
+    elif c3==2 and c6==2 and c9==2:
+        return
+    elif c1==2 and c5==2 and c9==2:
+        return
+    elif c3==2 and c5==2 and c7==2:
+        return
+    else:
+        print("manche suivante")
+
+if c1 != 0 and c2 !=0 and c3 !=0 and c4 !=0 and c5 !=0 and c6 !=0 and c7 !=0 and c8 !=0 and c9 !=0:
+    print
+    

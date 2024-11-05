c62cfbb5fbb0b2c745d412ebbdc77cee2ed6ba67
jeffry-khalife <jeffry.khalife@laplateforme.io>
2024-11-05 14:45:11 +0100

    update

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-test
+import tkinter
 
+root = tkinter.Tk()
 
-test
+def draw_grid():
+    button = tkinter.Button(root,height=5,width=20)
+    button.place(x=100,y=200)
+    button = tkinter.Button(root,height=5,width=20)
+    button.place(x=300,y=200)
+    button = tkinter.Button(root,height=5,width=20)
+    button.place(x=500,y=200)
+    button = tkinter.Button(root,height=5,width=20)
+    button.place(x=100,y=400)
+    button = tkinter.Button(root,height=5,width=20)
+    button.place(x=300,y=400)
+    button = tkinter.Button(root,height=5,width=20)
+    button.place(x=500,y=400)
+    button = tkinter.Button(root,height=5,width=20)
+    button.place(x=100,y=600)
+    button = tkinter.Button(root,height=5,width=20)
+    button.place(x=300,y=600)
+    button = tkinter.Button(root,height=5,width=20)
+    button.place(x=500,y=600)
+    
 
-
-test.
+root.title("tictac")
+root.minsize(700,700)
+draw_grid()
+root.mainloop()
